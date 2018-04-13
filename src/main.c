03cf46a0851f5cc6c1bc8c3176833edbd5a364cd
Michał Piotr Stankiewicz <ms335789@students.mimuw.edu.pl>
2018-04-13 15:35:37 +0200

    segmentation fault version

--- src/main.c
+++ src/main.c
@@ -9,22 +9,106 @@
 #include <sys/syscall.h>
 #include <errno.h>
 #include <inttypes.h>
+#include <elf.h>
+#include <stdio.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #include "../include/alienos.h"
 #include "../include/debug.h"
 #include "../include/display.h"
 
-int main(int argc, char **argv) {
-    pid_t child;
-    struct user_regs_struct regs;
-    int status;
-    long r;
-    int was_execve = 0;
-    int enter = 0;
-    int exit_status = EXIT_SUCCESS;
-    void* localv;
+int change_elf(pid_t child, int argc, char **argv) {
+    int r = 0;
+    int found = -1;
+    struct stat st;
+    int f;
+    char *p;
+    if (stat(argv[1], &st) != 0) {
+        r = 1;
+        goto change_elf_end;
+    }
+
+    f = open(argv[1], O_RDONLY);
+    if (f == 0) {
+        r = 1;
+        goto change_elf_end;
+    }
+    p = mmap(0, (size_t) st.st_size, PROT_READ, MAP_PRIVATE, f, 0);
+    if (p == 0) {
+        r = 1;
+        goto change_elf_file_close_end;
+    }
+
+    Elf64_Ehdr *ehdr = (Elf64_Ehdr*) p;
+    Elf64_Phdr *phdr = (Elf64_Phdr*) (p + ehdr->e_phoff);
+    int phnum = ehdr->e_phnum;
+
+    Elf64_Phdr *ph_strtab = &phdr[ehdr->e_phentsize];
+    const char *const ph_strtab_p = p + ph_strtab->p_offset;
+
+    for (int i = 0; i < phnum; i++) {
+        if (phdr[i].p_type == PT_PARAMS) {
+            found = i;
+            break;
+        }
+    }
+
+    if (found == -1) {
+        r = 1;
+        goto change_elf_p_end;
+    }
+
+    debug("Section number is %d\n"
+          "\ttype: %x\n"
+          "\tflags: %x\n"
+          "\toffset: %x\n"
+          "\tpaddr: %x\n"
+          "\tfilesz: %x\n"
+          "\tmemsz: %x\n"
+          "\t\tArguments number %d\n"
+          "\talign: %x\n",
+          found,
+          phdr[found].p_type,
+          phdr[found].p_flags,
+          phdr[found].p_offset,
+          phdr[found].p_paddr,
+          phdr[found].p_filesz,
+          phdr[found].p_memsz,
+          phdr[found].p_memsz / 4,
+          phdr[found].p_align
+    );
+
+    int32_t* localv;
     struct iovec local_cpy;
     struct iovec remote_cpy;
+
+    localv = (int32_t*) malloc(phdr[found].p_memsz);
+
+    for (int i = 0; (i < phdr[found].p_memsz / 4) && (i < argc - 2); i++) {
+        int32_t n = (int32_t) strtoll(argv[i + 2], NULL, 10);
+        localv[i] = n;
+    }
+
+    local_cpy.iov_base = localv;
+    local_cpy.iov_len = phdr[found].p_memsz;
+    remote_cpy.iov_base = (void*) phdr[found].p_paddr;
+    remote_cpy.iov_len = local_cpy.iov_len;
+    process_vm_writev(child, &local_cpy, 1, &remote_cpy, 1, 0);
+
+change_elf_p_end:
+    munmap(p, (size_t) st.st_size);
+change_elf_file_close_end:
+    close(f);
+change_elf_end:
+    return r;
+}
+
+int main(int argc, char **argv) {
+    pid_t child;
+    long r;
+    int exit_status = EXIT_SUCCESS;
 
     child = fork();
 
@@ -41,6 +125,13 @@ int main(int argc, char **argv) {
             exit(1);
         }
     } else {
+        void* localv;
+        struct iovec local_cpy;
+        struct iovec remote_cpy;
+        int was_execve = 0;
+        int enter = 0;
+        int status;
+        struct user_regs_struct regs;
         if (display_init() != 0 ) {
             goto fail;
         }
@@ -74,13 +165,15 @@ int main(int argc, char **argv) {
                         if (display_read_char(&c) != 0) {
                             goto fail;
                         }
-                        r = ptrace(PTRACE_POKEUSER, child, RAX * 8, c);
+                        r = ptrace(PTRACE_POKEUSER, child, RAX
+                                                           * 8, c);
                         if ( r == -1 ) {
                             goto fail;
                         }
                         enter = 0;
                         break;
                     case ALIENOS_PRINT:
+                        debug("print(x=%llu, y=%llu)\n", regs.rdi, regs.rsi);
                         localv = malloc(sizeof(uint16_t) * regs.r10);
                         local_cpy.iov_base = localv;
                         local_cpy.iov_len = sizeof(uint16_t) * regs.r10;
@@ -95,9 +188,13 @@ int main(int argc, char **argv) {
                         display_move_cursor((int) regs.rdi, (int) regs.rsi);
                         break;
                     case 59:
+                        if (change_elf(child, argc, argv) != 0) {
+                            goto fail;
+                        }
                         if (was_execve == 0) {
                             was_execve = 1;
                         } else {
+                            errno = EINVAL;
                             goto fail;
                         }
                         break;
@@ -105,8 +202,9 @@ int main(int argc, char **argv) {
                         debug("Unknown %llu\n", regs.rax);
                 }
             } else {
-                // LEAVE
-                // DO STUFF !!!!! TODO
+                if (was_execve == 1) {
+                    was_execve = 2;
+                }
                 enter = 0;
             }
             r = ptrace(PTRACE_SYSCALL, child, NULL, NULL);
